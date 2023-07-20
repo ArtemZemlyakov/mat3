7f197801182ebca0764a47b7e0dfb68ba5bbd356
ArtemZemlyakov <139268188+ArtemZemlyakov@users.noreply.github.com>
2023-07-20 18:25:30 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+if (confirm('JavaScript появился в 1995 году?')){
+alert('Верно')
+}else {
+alert ('JavaScript появился в 1995 году.')
+};
+
+if (confirm('Спецификация JavaScript называется ECMAScript?')){
+alert('Верно')
+}else {
+alert ('Спецификация JavaScript называется ECMAScript.')
+};
+
+if (confirm('JavaScript был создан за 1 месяц??')){
+alert('JavaScript был создан за 10 дней.')
+}else {
+alert ('Верно')
+};
